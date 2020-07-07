92ba028a68668f5e025f7aeab1929fe7eba9d61a
Max Dolensky <mdolensky0@gmail.com>
2020-07-06 19:43:02 -0700

    Initial Commit

---
+++ Aura/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Aura
+//
+//  Created by Max Dolensky on 7/6/20.
+//  Copyright © 2020 Max Dolensky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
