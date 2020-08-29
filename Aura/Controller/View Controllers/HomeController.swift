4af22bae3275c5d8377758059e6bc246fb3ea10a
Max Dolensky <mdolensky0@gmail.com>
2020-08-28 18:53:12 -0700

    Basic Home Screen Finished

--- Aura/Controller/View Controllers/HomeController.swift
+++ Aura/Controller/View Controllers/HomeController.swift
@@ -10,45 +10,222 @@ import UIKit
 
 class HomeController: UIViewController {
 
-    var comingSoon: UILabel = {
+    var titleLabel: UILabel = {
         
-        let label = UILabel()
-        let text = NSMutableAttributedString(string: "Aura")
-        text.addAttribute(.foregroundColor, value: K.Colors.purple, range: NSRange(location: 0, length: 2))
-        text.addAttribute(.foregroundColor, value: K.Colors.darkGrey, range: NSRange(location: 2, length: 1))
-        text.addAttribute(.foregroundColor, value: K.Colors.lightPink, range: NSRange(location: 3, length: 1))
-        text.addAttribute(.font, value: UIFont.systemFont(ofSize: 60, weight: .bold), range: NSRange(location: 0, length: text.length))
-        label.numberOfLines = 1
+        let label = UILabel(frame: CGRect(x: 10, y: 0, width: 50, height: 30))
+        label.backgroundColor = .clear
+        label.font = UIFont(name: K.Fonts.avenirBlack, size: 17)
+        label.text = "Aura"
+        label.numberOfLines = 2
+        label.textColor = .white
         label.textAlignment = .center
-        label.attributedText = text
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
         
     }()
     
-    var referLabel: UILabel = {
+    var searchView: UIView = {
         
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 30, weight: .regular)
-        label.text = "start at search tab"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
+        let container = UIView()
+        
+        let button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "TheSearch"), for: .normal)
+        button.contentMode = .scaleAspectFit
+        button.roundCorners(cornerRadius: 10)
+        button.addTarget(self, action: #selector(goToSearch), for: .touchUpInside)
+        
+        container.addSubview(button)
+        button.anchor(top: container.topAnchor,
+                      bottom: container.bottomAnchor,
+                      leading: container.leadingAnchor,
+                      trailing: container.trailingAnchor,
+                      height: nil,
+                      width: nil)
+        
+        return container
+        
+    }()
+    
+    var keyView: UIView = {
+        
+        let container = UIView()
+        
+        let button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "TheKey"), for: .normal)
+        button.contentMode = .scaleAspectFit
+        button.roundCorners(cornerRadius: 10)
+        button.addTarget(self, action: #selector(goToKey), for: .touchUpInside)
+        
+        container.addSubview(button)
+        button.anchor(top: container.topAnchor,
+                      bottom: container.bottomAnchor,
+                      leading: container.leadingAnchor,
+                      trailing: container.trailingAnchor,
+                      height: nil,
+                      width: nil)
+        
+        return container
+        
+    }()
+    
+    var flashcardsView: UIView = {
+        
+        let container = UIView()
+        
+        let button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "TheFlashcards"), for: .normal)
+        button.contentMode = .scaleAspectFit
+        button.roundCorners(cornerRadius: 10)
+        button.addTarget(self, action: #selector(goToFlashcards), for: .touchUpInside)
+        
+        container.addSubview(button)
+        button.anchor(top: container.topAnchor,
+                      bottom: container.bottomAnchor,
+                      leading: container.leadingAnchor,
+                      trailing: container.trailingAnchor,
+                      height: nil,
+                      width: nil)
+        
+        return container
+        
+    }()
+    
+    var lessonsView: UIView = {
+        
+        let container = UIView()
+        
+        let button = UIButton()
+        button.setImage(#imageLiteral(resourceName: "TheLessons"), for: .normal)
+        button.contentMode = .scaleAspectFit
+        button.roundCorners(cornerRadius: 10)
+        button.addTarget(self, action: #selector(goToLessons), for: .touchUpInside)
+        
+        container.addSubview(button)
+        button.anchor(top: container.topAnchor,
+                      bottom: container.bottomAnchor,
+                      leading: container.leadingAnchor,
+                      trailing: container.trailingAnchor,
+                      height: nil,
+                      width: nil)
+        
+        return container
         
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .white
         
-        view.addSubview(comingSoon)
-        view.addSubview(referLabel)
-        comingSoon.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        comingSoon.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        referLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        referLabel.topAnchor.constraint(equalTo: comingSoon.topAnchor, constant: 100).isActive = true
+        setup()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        searchView.setShadow(color: .black, opacity: 0.3, offset: CGSize(width: 5, height: 5), radius: 5, cornerRadius: 10)
+        
+        keyView.setShadow(color: .black, opacity: 0.3, offset: CGSize(width: 5, height: 5), radius: 5, cornerRadius: 10)
+        
+        flashcardsView.setShadow(color: .black, opacity: 0.3, offset: CGSize(width: 5, height: 5), radius: 5, cornerRadius: 10)
+        
+        lessonsView.setShadow(color: .black, opacity: 0.3, offset: CGSize(width: 5, height: 5), radius: 5, cornerRadius: 10)
+        
+    }
+    
+    func setup() {
+        
+        view.backgroundColor = K.DesignColors.background
+        
+        // Add Center Title
+        self.navigationItem.titleView = titleLabel
+        
+        // Add user button
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person"), style: .plain,
+                                                                 target: self, action: #selector(profileButtonTapped))
+        
+        // Make bar color purple, and buttons white
+        self.navigationController?.navigationBar.tintColor = .white
+        self.navigationController?.navigationBar.barTintColor = K.DesignColors.primary
+        
+        view.addSubview(searchView)
+        view.addSubview(keyView)
+        view.addSubview(flashcardsView)
+        view.addSubview(lessonsView)
+        
+        searchView.anchor(top: view.safeAreaLayoutGuide.topAnchor,
+                          bottom: nil,
+                          leading: view.leadingAnchor,
+                          trailing: view.trailingAnchor,
+                          height: 157,
+                          width: nil,
+                          padding: UIEdgeInsets(top: 20, left: 20, bottom: 0, right: -20))
+        
+        keyView.anchor(top: searchView.bottomAnchor,
+                       bottom: nil,
+                       leading: searchView.leadingAnchor,
+                       trailing: nil,
+                       height: 157,
+                       width: nil,
+                       padding: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0))
+        
+        keyView.widthAnchor.constraint(equalTo: searchView.widthAnchor, multiplier: 0.5, constant: -10).isActive = true
+        
+        flashcardsView.anchor(top: searchView.bottomAnchor,
+                       bottom: nil,
+                       leading: nil,
+                       trailing: searchView.trailingAnchor,
+                       height: 157,
+                       width: nil,
+                       padding: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0))
+        
+        flashcardsView.widthAnchor.constraint(equalTo: searchView.widthAnchor, multiplier: 0.5, constant: -10).isActive = true
+        
+        lessonsView.anchor(top: keyView.bottomAnchor,
+                           bottom: nil,
+                           leading: searchView.leadingAnchor,
+                           trailing: searchView.trailingAnchor,
+                           height: 157,
+                           width: nil,
+                           padding: UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0))
+    }
+    
+    @objc func goToSearch() {
+        
+        self.tabBarController?.selectedIndex = 1
+        
+    }
+    
+    @objc func goToKey() {
+        
+        self.tabBarController?.selectedIndex = 2
+        
+    }
+    
+    @objc func goToFlashcards() {
+        
+        self.tabBarController?.selectedIndex = 3
+        
+    }
+    
+    @objc func goToLessons() {
+        
+        self.tabBarController?.selectedIndex = 4
+        
+    }
+    
+    @objc func profileButtonTapped() {
+        
+        if Utilities.shared.isUserSignedIn {
+            
+            Utilities.shared.signUserOut(alertIn: self)
+            
+        }
+        
+        else {
+            
+            let vc = UINavigationController(rootViewController: LoginController())
+            let login = vc.viewControllers[0] as! LoginController
+            login.isModal = true
+            self.present(vc, animated: true, completion: nil)
+            
+        }
         
     }
 }
